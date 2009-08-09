bfbed55c97c36ede18c8bfa2669aa9dc6a26057d
Christian Mayer <git@mifix.com>
2009-08-09 18:56:06 +0200

    * nest statisctic under player * tiny css * fetch stats when creating a player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,14 @@
 class Player < ActiveRecord::Base
+  has_many :statistics
+  
+  after_create :fetch_statistics
+  
+  
+  protected
+  
+  def fetch_statistics
+    fetcher = StatisticFetcher.new(name)
+    statistics.build.insert_data_from_website fetcher.data
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,35 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090809135517) do
+ActiveRecord::Schema.define(:version => 20090809142724) do
 
   create_table "players", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statistics", :force => true do |t|
+    t.integer  "player_id"
+    t.integer  "rank"
+    t.float    "kd"
+    t.integer  "exp"
+    t.integer  "gp"
+    t.integer  "kills"
+    t.integer  "deaths"
+    t.integer  "headshots"
+    t.integer  "matches"
+    t.integer  "matches_won"
+    t.integer  "matches_lost"
+    t.integer  "matches_forfeited"
+    t.float    "kills_per_match"
+    t.float    "deaths_per_match"
+    t.integer  "kill_streak"
+    t.integer  "multi_kills"
+    t.integer  "ultra_kills"
+    t.integer  "fantastic"
+    t.integer  "unbelievable"
+    t.integer  "unbelievable_plus"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
