1fd9254bda1c4c584a259e1dad292ed778de2d87
Christian Mayer <git@mifix.com>
2009-08-10 23:51:05 +0200

    Show and format daily stats

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,2 +1,31 @@
 module StatisticsHelper
+  
+  def progress(current, previous, attr)
+    prev_value = previous.send(attr) 
+    return 0 if prev_value.nil? 
+    
+    current.send(attr) - prev_value
+  end
+  
+  def format_progress(current, previous, attr)
+    val = progress(current, previous, attr)
+    format_value(val)
+  end
+  
+  def format_value(value)
+    if value > 0
+      "<div class='pos'>#{format_number(value)}</div>"
+    elsif value < 0
+      "<div class='neg'>#{format_number(value)}</div>"
+    end
+  end
+
+  def format_number(value)
+    if value.class == Float
+      number_with_precision(value, 2) 
+    else
+      value
+    end
+  end
+  
 end
